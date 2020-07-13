3cd6275102fe0aebfae6ea8c33efc6d5c92c313a
luizdopc <luizotaviosoares@outlook.com>
2020-07-13 15:13:02 -0300

    Add patch files and script

---
+++ script-patch.sh
@@ -0,0 +1,3 @@
+patch -t ./node_modules/react-native-material-textfield/src/components/affix/index.js < ./patches/react-native-material-textfield/affix/index.patch
+patch -t ./node_modules/react-native-material-textfield/src/components/helper/index.js < ./patches/react-native-material-textfield/helper/index.patch
+patch -t ./node_modules/react-native-material-textfield/src/components/label/index.js < ./patches/react-native-material-textfield/label/index.patch
